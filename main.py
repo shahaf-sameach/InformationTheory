40ace367c8fa1f2e50e9f37615ae1b28834cec20
shahaf <shahaf@minute.ly>
2018-08-18 12:19:02 +0300

    init repo

---
+++ main.py
@@ -0,0 +1,17 @@
+from compression.huffman import HuffmanCoding
+
+# if __name__ == "__main__":
+input_data = ''
+with open("dickens.txt", errors="ignore") as f:
+    input_data = f.read().strip()
+
+
+h = HuffmanCoding()
+
+compress = h.compress(input_data)
+print(len(compress))
+print(len(compress) / len(input_data))
+decoded = h.decompress(compress)
+print(decoded == input_data)
+
+
